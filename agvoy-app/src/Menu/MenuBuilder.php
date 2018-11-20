270a984bcd6bb2226ad0a501381555744ed43a2b
Nayht <william.nayht@telecom-sudparis.eu>
2018-11-20 12:42:21 +0100

    Refonte du menu pour que les personnes ayant des droits supérieurs aient accès aux liens des droits inférieurs

--- agvoy-app/src/Menu/MenuBuilder.php
+++ agvoy-app/src/Menu/MenuBuilder.php
@@ -27,96 +27,12 @@ class MenuBuilder
         $this->factory = $factory;
         $this->container = $container;
     }
-    
-    public function createMainMenu(array $options)
-    {
-        $menu = $this->factory->createItem('root');
-        $menu->setChildrenAttribute('class', 'nav navbar-nav');
-        
-        $menu->addChild('Accueil', array('route' => 'home'))
-            ->setAttributes(array(
-                'class' => 'nav-link',
-                'icon' => 'fa fa-list'
-            ));
 
-        $menu->addChild('Circuits', array('route' => 'circuits'))
-            ->setAttributes(array(
-                'class' => 'nav-link',
-                'icon' => 'fa fa-list'
-            ));
-
-        $isConnected=false;
-        if($this->container->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
-            $isConnected=true;
-        }
-
-        if($isConnected) {
-            // Children for connected users
-            $menu->addChild('Edit circuits', array('route' => 'admin_circuit_index'))
-                ->setAttributes(array(
-                    'class' => 'nav-link',
-                    'icon' => 'fa fa-list'
-                ));
-            $menu->addChild('Edit étapes', array('route' => 'admin_etape_index'))
-                ->setAttributes(array(
-                    'class' => 'nav-link',
-                    'icon' => 'fa fa-list'
-                ));
-        }
-
-        return $menu;
-    }
-    
-    public function createUserMenu(array $options)
-    {
-        $menu = $this->factory->createItem('root');
-        $menu->setChildrenAttribute('class', 'nav navbar-nav ml-auto');
-        
-        //if($this->container->get('security.context')->isGranted(array('ROLE_ADMIN', 'ROLE_USER'))) {} // Check if the visitor has any authenticated roles
-        $isConnected=false;
-        if($this->container->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
-            $isConnected=true;
-        }
-
-        if($isConnected)
-        {
-            // Get username of the current logged in user
-            $username = $this->container->get('security.token_storage')->getToken()->getUser()->getUsername();
-            $label = 'Hi '. $username;
-        }
-        else 
-        {
-            $label = 'Hi visitor';
-        }
-
-        $menu->addChild('User', array('label' => $label))
-        ->setAttribute('class', 'usernameDisplay');
-
-        if($isConnected) {
-            $menu->addChild('Disconnect', array('route' => 'fos_user_security_logout'))
-                ->setAttributes(array(
-                    'class' => 'nav-link',
-                    'icon' => 'fa fa-list'
-                ));
-        }
-        else{
-            $menu->addChild('Connexion', array('route' => 'fos_user_security_login'))
-                ->setAttributes(array(
-                    'class' => 'nav-link',
-                    'icon' => 'fa fa-list'
-                ));
-        }
-
-
-        return $menu;
-    }
-
-    public function createAdminMenu(array $options)
-    {
+    public function createConnexionMenu(array $options){
         $menu = $this->factory->createItem('root');
         $menu->setChildrenAttribute('class', 'nav navbar-nav ml-auto');
 
-        //if($this->container->get('security.context')->isGranted(array('ROLE_ADMIN', 'ROLE_USER'))) {} // Check if the visitor has any authenticated roles
+
         $isConnected=false;
         if($this->container->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
             $isConnected=true;
@@ -150,6 +66,52 @@ class MenuBuilder
                     'icon' => 'fa fa-list'
                 ));
         }
+
+        return $menu;
+
+    }
+    
+    public function createBaseMenu(array $options)
+    {
+        $menu = $this->factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'nav navbar-nav');
+        
+        $menu->addChild('Accueil', array('route' => 'home'))
+            ->setAttributes(array(
+                'class' => 'nav-link',
+                'icon' => 'fa fa-list'
+            ));
+
+        $menu->addChild('Circuits', array('route' => 'circuits'))
+            ->setAttributes(array(
+                'class' => 'nav-link',
+                'icon' => 'fa fa-list'
+            ));
+        return $menu;
+    }
+    
+    public function createUserMenu(array $options)
+    {
+        $menu = $this->createBaseMenu($options);
+
+        return $menu;
+    }
+
+    public function createAdminMenu(array $options)
+    {
+        $menu = $this->createUserMenu($options);
+
+        // Children for connected users
+        $menu->addChild('Edit circuits', array('route' => 'admin_circuit_index'))
+            ->setAttributes(array(
+                'class' => 'nav-link',
+                'icon' => 'fa fa-list'
+            ));
+        $menu->addChild('Edit étapes', array('route' => 'admin_etape_index'))
+            ->setAttributes(array(
+                'class' => 'nav-link',
+                'icon' => 'fa fa-list'
+            ));
 
 
         return $menu;
